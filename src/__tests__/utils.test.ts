bc49b18415fead38a55fb12286d8e80ef82b9e6a
余小波 <yuxiaobo64@gmail.com>
2024-09-04 11:38:41 +0800

    fixed 修复了一些问题

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,8 +1,8 @@
 import { gcj02_to_bd09 } from '../utils';
 
 test('GCJ02-TO-BD09', () => {
-    const lnglat = [106.697845, 26.567703] as any // 花果园南广场，GCJ-02
-    const bd_lnglat = [106.70430927991049, 26.573896142574362];
+  const lnglat = [106.697845, 26.567703] as any; // 花果园南广场，GCJ-02
+  const bd_lnglat = [106.70430927991049, 26.573896142574362];
 
-    expect(gcj02_to_bd09(lnglat)).toEqual(bd_lnglat); // 花果园南广场，BD-09
+  expect(gcj02_to_bd09(lnglat)).toEqual(bd_lnglat); // 花果园南广场，BD-09
 });
